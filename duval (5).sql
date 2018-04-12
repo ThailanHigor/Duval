75d1efdded78c370be2dbeddc4ad5bf8d89d3d2a
Thailan <thailan-higor@hotmail.com>
2018-04-11 23:31:12 -0300

    Alteracoes no banco

--- duval (5).sql
+++ duval (5).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 09-Abr-2018 às 23:38
--- Versão do servidor: 10.1.28-MariaDB
--- PHP Version: 7.1.10
+-- Generation Time: 12-Abr-2018 às 04:30
+-- Versão do servidor: 10.1.30-MariaDB
+-- PHP Version: 7.2.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -42,8 +42,9 @@ CREATE TABLE `caixa` (
 --
 
 INSERT INTO `caixa` (`id_caixa`, `caixa_data`, `valor_abertura`, `valor_atual`, `valor_fechamento`, `caixa_status`) VALUES
-(1, '2018-04-09', '30.00', '30.00', '30.00', 1),
-(2, '2018-04-09', '5.00', '5.00', '5.00', 1);
+(7, '2018-04-11', '100.00', '700.00', '700.00', 1),
+(8, '2018-04-11', '100.00', '100.00', '100.00', 1),
+(9, '2018-04-11', '100.00', '220.00', '0.00', 0);
 
 -- --------------------------------------------------------
 
@@ -53,12 +54,12 @@ INSERT INTO `caixa` (`id_caixa`, `caixa_data`, `valor_abertura`, `valor_atual`, 
 
 CREATE TABLE `fornecedores` (
   `id` int(11) NOT NULL,
-  `nome` varchar(100) NOT NULL,
+  `nome` varchar(30) NOT NULL,
   `representante` varchar(100) DEFAULT 'Nao possui',
-  `rua` varchar(100) NOT NULL,
-  `bairro` varchar(100) NOT NULL,
+  `rua` varchar(20) NOT NULL,
+  `bairro` varchar(70) NOT NULL,
   `numero` varchar(10) NOT NULL,
-  `cidade` varchar(100) NOT NULL,
+  `cidade` varchar(30) NOT NULL,
   `uf` varchar(2) NOT NULL,
   `complemento` varchar(100) NOT NULL,
   `cep` varchar(30) NOT NULL,
@@ -74,8 +75,7 @@ CREATE TABLE `fornecedores` (
 --
 
 INSERT INTO `fornecedores` (`id`, `nome`, `representante`, `rua`, `bairro`, `numero`, `cidade`, `uf`, `complemento`, `cep`, `cnpj`, `inscricaoEstadual`, `telefone1`, `telefone2`, `email`) VALUES
-(39, 'ArtWeb', 'Rodrigo Rodrigues e Thailan Higor', 'Av. Almirante Adalberto de Barros Nunes', 'Vila Mury', '1326', 'Volta Redonda', 'RJ', 'Apt 202', '27281800', '15585876759', '15585876759', '24999713157', '24999084826', 'artwebvr@hotmail.com'),
-(40, 'Rodrigo wquhdiuwqhf 1 soihdsaihfsuafhsa4 fioasoj 5', 'oijdasoifhsahf6 doisajdiosajdoa 9 jdsoijdoaisdas 8', 'jsdaljdaskldsad 0 dasiodjsaidjas 8', 'kldjksajkldjs 0 idhasiudhsa1 daishdsuiahd 2', '256', 'kjhdjshsajdkhsjkas ashdadsakdkj2', 'rj', 'apt 20265', '275866523266', '2132156465765132134646542133', '1312465498723132', '21343465498721', '21321564489765', 'ohdqwidhwqfjqw@fsjoifjasiofhasekmgads.com.brr');
+(34, 'GenerososA', 'ninguemA', 'leilaA', 'pjA', '30', 'vr', 'RA', 'cada da ruaA', '2831-A', '123123.432A', '34324.23423A', '24 9993492A', '24 3343489A', 'thailan@hotmil.comA');
 
 -- --------------------------------------------------------
 
@@ -89,6 +89,13 @@ CREATE TABLE `nota_fiscal` (
   `fornecedor` int(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `nota_fiscal`
+--
+
+INSERT INTO `nota_fiscal` (`id_NF`, `cod_NF`, `fornecedor`) VALUES
+(31, '340', 34);
+
 -- --------------------------------------------------------
 
 --
@@ -96,13 +103,13 @@ CREATE TABLE `nota_fiscal` (
 --
 
 CREATE TABLE `produtos` (
-  `id` int(50) NOT NULL,
-  `codigo` varchar(50) NOT NULL,
+  `id` int(11) NOT NULL,
+  `codigo` varchar(15) NOT NULL,
   `nome` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `quantidade` int(50) NOT NULL,
+  `quantidade` int(11) NOT NULL,
   `precoCompra` decimal(5,2) NOT NULL,
   `precoVenda` decimal(5,2) NOT NULL,
-  `idforn` int(50) NOT NULL
+  `idforn` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -110,8 +117,7 @@ CREATE TABLE `produtos` (
 --
 
 INSERT INTO `produtos` (`id`, `codigo`, `nome`, `quantidade`, `precoCompra`, `precoVenda`, `idforn`) VALUES
-(1, '7895421154235', 'Smart Phone 4G 16GB de Ram e Memoria', 0, '25.00', '650.00', 39),
-(2, '7895465898756', 'Notebook 123 Dell Samsung Fonte de Vida', 0, '25.00', '265.00', 40);
+(62, '1234567891234', 'Carvao', -31, '30.00', '40.00', 34);
 
 -- --------------------------------------------------------
 
@@ -120,11 +126,18 @@ INSERT INTO `produtos` (`id`, `codigo`, `nome`, `quantidade`, `precoCompra`, `pr
 --
 
 CREATE TABLE `produtos_nf` (
-  `id_produtos_nf` int(50) NOT NULL,
-  `produto_NF` int(50) NOT NULL,
-  `quantidade` int(50) NOT NULL,
-  `id_NF` int(50) NOT NULL
+  `id_produtos_nf` int(11) NOT NULL,
+  `produto_NF` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `id_NF` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `produtos_nf`
+--
+
+INSERT INTO `produtos_nf` (`id_produtos_nf`, `produto_NF`, `quantidade`, `id_NF`) VALUES
+(10, 62, 3, 31);
 
 -- --------------------------------------------------------
 
@@ -133,11 +146,36 @@ CREATE TABLE `produtos_nf` (
 --
 
 CREATE TABLE `produtos_venda` (
-  `id_prod_venda` int(50) NOT NULL,
-  `prod_venda` int(50) NOT NULL,
-  `qtd_venda` int(50) NOT NULL,
-  `id_Venda` int(50) NOT NULL
+  `id_prod_venda` int(11) NOT NULL,
+  `prod_venda` int(11) NOT NULL,
+  `qtd_venda` int(11) NOT NULL,
+  `id_Venda` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `produtos_venda`
+--
+
+INSERT INTO `produtos_venda` (`id_prod_venda`, `prod_venda`, `qtd_venda`, `id_Venda`) VALUES
+(14, 62, 1, 21),
+(15, 62, 1, 22),
+(16, 62, 1, 23),
+(17, 62, 1, 24),
+(18, 62, 1, 25),
+(19, 62, 2, 26),
+(20, 62, 1, 27),
+(21, 62, 4, 28),
+(22, 62, 1, 29),
+(23, 62, 5, 30),
+(24, 62, 1, 31),
+(25, 62, 3, 32),
+(26, 62, 3, 33),
+(27, 62, 1, 34),
+(28, 62, 4, 35),
+(29, 62, 1, 36),
+(30, 62, 1, 37),
+(31, 62, 2, 37),
+(32, 62, 3, 38);
 
 -- --------------------------------------------------------
 
@@ -151,6 +189,30 @@ CREATE TABLE `venda` (
   `valor_total_venda` varchar(30) NOT NULL,
   `form_pgmto` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `venda`
+--
+
+INSERT INTO `venda` (`id_Venda`, `data_Venda`, `valor_total_venda`, `form_pgmto`) VALUES
+(21, '2018-04-08 19:47:00', '40.00', 'Outro'),
+(22, '2018-04-08 19:51:00', '40.00', 'Cartao'),
+(23, '2018-04-08 21:29:00', '40.00', 'Cartao'),
+(24, '2018-04-08 22:00:00', '40.00', 'Cartao'),
+(25, '2018-04-08 22:13:00', '40.00', 'Cartao'),
+(26, '2018-04-08 22:13:00', '80.00', 'Dinheiro'),
+(27, '2018-04-08 22:36:00', '40.00', 'Outro'),
+(28, '2018-04-08 22:46:00', '160.00', 'Cartao'),
+(29, '2018-04-08 22:47:00', '40.00', 'Cartao'),
+(30, '2018-04-08 22:48:00', '200.00', 'Cartao'),
+(31, '2018-04-08 22:49:00', '40.00', 'Cartao'),
+(32, '2018-04-11 21:46:00', '120.00', 'Cartao'),
+(33, '2018-04-11 21:56:00', '120.00', 'Cartao'),
+(34, '2018-04-11 21:56:00', '40.00', 'Outro'),
+(35, '2018-04-11 21:58:00', '160.00', 'Outro'),
+(36, '2018-04-11 22:06:00', '40.00', 'Cartao'),
+(37, '2018-04-11 22:11:00', '120.00', 'Cartao'),
+(38, '2018-04-11 22:37:00', '120.00', 'Cartao');
 
 --
 -- Indexes for dumped tables
@@ -210,43 +272,43 @@ ALTER TABLE `venda`
 -- AUTO_INCREMENT for table `caixa`
 --
 ALTER TABLE `caixa`
-  MODIFY `id_caixa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_caixa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `fornecedores`
 --
 ALTER TABLE `fornecedores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT for table `nota_fiscal`
 --
 ALTER TABLE `nota_fiscal`
-  MODIFY `id_NF` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_NF` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 
 --
 -- AUTO_INCREMENT for table `produtos_nf`
 --
 ALTER TABLE `produtos_nf`
-  MODIFY `id_produtos_nf` int(50) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_produtos_nf` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `produtos_venda`
 --
 ALTER TABLE `produtos_venda`
-  MODIFY `id_prod_venda` int(50) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_prod_venda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT for table `venda`
 --
 ALTER TABLE `venda`
-  MODIFY `id_Venda` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_Venda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- Constraints for dumped tables
